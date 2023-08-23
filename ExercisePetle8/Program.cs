7cea66dd958f2d19ef0f4d935ad9259c84f8e8bd
ACTFYZZ <ACTFYZZ@PL10006677>
2023-08-23 12:57:31 +0200

    Add project files.

---
+++ ExercisePetle8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace ExercisePetle8
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            string a = "";
+            string b = "";
+            Console.WriteLine("Wpisz dowolny tekst");
+            a = Console.ReadLine();
+            //Creating inverted string
+            for (int i = a.Length-1; i >= 0; i--)
+            {
+                b = b + a[i];               
+            }
+            Console.WriteLine(b);
+        }
+    }
+}
